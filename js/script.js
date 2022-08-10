9f78fb4076a8a399b1fb37a9b593181022b72659
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-08-10 13:43:21 +0530

    Added Typing animation and write javascript with sript.js file.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
-$(document).ready(function (){
-    $(window).scroll(function (){
-        if (this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else {
+        } else {
             $('.navbar').removeClass("sticky");
         }
     })
 
     /* toggle menu/ navbar script */
 
-    $('.menu-btn').click(function (){
+    $('.menu-btn').click(function () {
 
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
 
     });
+
+    /*typing animation script*/
+    var typed = new Typed(".typing",{
+        strings:["Full Stack Developer","WEB-DEVELOPER","BACK-END DEVELOPER","FRONT-END DEVELOPER","SOFTWARE DESIGNER","UI/UX DESIGNER"],
+        typeSpeed:80,
+        backSpeed:50,
+        loop:true
+    });
+
 
 });
