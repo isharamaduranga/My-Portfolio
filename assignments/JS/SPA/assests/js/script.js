269d25ddfe9e2f327464c3364410d7ee4d20035a
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-09-21 00:19:25 +0530

    Updated content 'script.js' jquery file with js directory...

--- assignments/JS/SPA/assests/js/script.js
+++ assignments/JS/SPA/assests/js/script.js
@@ -46,7 +46,7 @@
 
   });
 
-  /* Displayed a Item form  When clicked "Item" link with navbar  */
+  /* Displayed a Order form  When clicked "Order" link with navbar  */
   $("#linkOrder,#linkOrder2").click(function (){
       $("#dashboard_Slide").css("display", "none");
       $("#customer_Slide").css("display", "none");
@@ -59,6 +59,22 @@
       $("#linkItem").css({'color': 'white','border-bottom':'none' });
       $("#linkOrder").css({'color':'deeppink','border-bottom':'2px solid gold'});
       $("#linkOrder_Details").css({'color': 'white','border-bottom':'none' });
+
+  });
+
+  /* Displayed a Order-Details form  When clicked "Order-Details" link with navbar  */
+  $("#linkOrder_Details,#linkOrder_Details2").click(function (){
+      $("#dashboard_Slide").css("display", "none");
+      $("#customer_Slide").css("display", "none");
+      $("#item_Slide").css("display", "none");
+      $("#order_Slide").css("display", "none");
+      $("#orderDetailsSlide").css("display", "block");
+
+      $("#linkHome").css({'color': 'white','border-bottom':'none' });
+      $("#linkCustomer").css({'color': 'white','border-bottom':'none' });
+      $("#linkItem").css({'color': 'white','border-bottom':'none' });
+      $("#linkOrder").css({'color': 'white','border-bottom':'none' });
+      $("#linkOrder_Details").css({'color':'deeppink','border-bottom':'2px solid gold'});
 
   });
 
