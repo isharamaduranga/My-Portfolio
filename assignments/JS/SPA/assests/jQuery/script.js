2b06c80e3d4b5d52f3216acf7931f498baddddd6
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-09-20 18:43:02 +0530

    content updated for When first opening the index file, hide all slides, but show only one content with script.js..

---
+++ assignments/JS/SPA/assests/jQuery/script.js
@@ -0,0 +1,17 @@
+
+  /* When first opening the index file, hide all slides, but show only one content. */
+
+  $("#dashboard_Slide").css("display", "block"); // firstly open
+  $("#customer_Slide").css("display", "none");
+  $("#item_Slide").css("display", "none");
+  $("#order_Slide").css("display", "none");
+  $("#orderDetailsSlide").css("display", "none");
+
+  $("#linkHome").css('color', 'crimson');
+  $("#linkCustomer").css('color', 'white');
+  $("#linkItem").css('color', 'white');
+  $("#linkOrder").css('color', 'white');
+  $("#linkPurchaseOrder").css('color', 'white');
+
+
+
