0ab7388b78806d9cc459d0719deab59f237f84c1
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-09-20 23:40:25 +0530

    Add Comments Script file..

--- assignments/JS/SPA/assests/js/script.js
+++ assignments/JS/SPA/assests/js/script.js
@@ -1,6 +1,7 @@
 
   /* When first opening the index file, hide all slides, but show only one content. */
 
+  /* Displayed  default DashBoard scripts..  */
   $("#dashboard_Slide").css("display", "block"); // firstly open
   $("#customer_Slide").css("display", "none");
   $("#item_Slide").css("display", "none");
@@ -13,7 +14,7 @@
   $("#linkOrder").css('color', 'white');
   $("#linkOrder_Details").css('color', 'white');
 
-
+  /* Displayed a customer form  When clicked "customer" link with navbar  */
   $("#linkCustomer").click(function () {
       $("#dashboard_Slide").css("display", "none");
       $("#customer_Slide").css("display", "block");
@@ -27,3 +28,15 @@
       $("#linkOrder").css('color', 'white');
       $("#linkOrder_Details").css('color', 'white');
   });
+
+  $("#linkItem").click( function (){
+
+
+
+
+
+
+
+  });
+
+
