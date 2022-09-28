673ec3099fdeb2d8bdb568ef974fd700763447ba
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-09-28 11:34:00 +0530

    select all the four text fields and then get their typed values with item_form..

--- assignments/CSS/CSS_Frameworks_POS/assests/jquery/script_item.js
+++ assignments/CSS/CSS_Frameworks_POS/assests/jquery/script_item.js
@@ -1,2 +1,10 @@
 
 
+$('#btnItemSave').click(function () {
+    let ItemCode = $('#txtItemCode').val();
+    let ItemName = $('#txtItemName').val();
+    let ItemPrice = $('#txtItemPrice').val();
+    let ItemQty = $('#txtItemQty').val();
+
+
+});
