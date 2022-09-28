e7627a6d94e2a32542240665e95a2d0f6385f3e0
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-09-28 11:40:49 +0530

    created a item object & put all of these values inside a named container..

--- assignments/CSS/CSS_Frameworks_POS/assests/jquery/script_item.js
+++ assignments/CSS/CSS_Frameworks_POS/assests/jquery/script_item.js
@@ -1,10 +1,19 @@
 
-
+/** Save  Item Click Event Function */
 $('#btnItemSave').click(function () {
+    /** select all the four text fields and then get their typed values */
     let ItemCode = $('#txtItemCode').val();
     let ItemName = $('#txtItemName').val();
     let ItemPrice = $('#txtItemPrice').val();
     let ItemQty = $('#txtItemQty').val();
 
+    /**  put all of these values inside a named container  */
+    var item={
+        code:ItemCode,
+        name:ItemName,
+        price:ItemPrice,
+        qty:ItemQty
+    }
+    console.log(item);
 
 });
