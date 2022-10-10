4256f8789e3f401bddfa9911baf37a6457acd803
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-10-10 23:23:43 +0530

    Content updated with selected combobox option functionalities..

--- assignments/JS/SPA/controller/OrderController.js
+++ assignments/JS/SPA/controller/OrderController.js
@@ -14,3 +14,18 @@ function loadAllItemsForComboBox() {
     }
 }
 
+$('#cmbCustomerIDS').change(function () {
+    let cusID=$('#cmbCustomerIDS').val()
+    let customer = searchCustomerForInputField(cusID);
+    if (customer != null) {
+
+        $('#txtCusIdForOrder').val(customer.id);
+        $('#txtCusNameForOrder').val(customer.name);
+        $('#txtCusSalaryForOrder').val(customer.salary);
+        $('#txtAddressForOrder').val(customer.address);
+    }
+});
+
+
+
+
