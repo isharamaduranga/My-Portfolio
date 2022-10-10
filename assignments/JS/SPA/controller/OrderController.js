62ef4bce4cbb8df1f82f388d392c1254c0d6e54d
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-10-11 00:50:52 +0530

    Content updated ...

--- assignments/JS/SPA/controller/OrderController.js
+++ assignments/JS/SPA/controller/OrderController.js
@@ -2,6 +2,7 @@
 
 function loadAllCustomersForComboBox() {
     $('#cmbCustomerIDS').empty();
+    $('#cmbCustomerIDS').prepend('<option>Select Customer</option>');
     for (let customer of customers) {
         $('#cmbCustomerIDS').append(`<option>${customer.id}</option>`);
     }
@@ -9,13 +10,14 @@ function loadAllCustomersForComboBox() {
 
 function loadAllItemsForComboBox() {
     $('#cmbItemIds').empty();
+    $('#cmbItemIds').prepend('<option>Select Item</option>');
     for (let item of items) {
         $('#cmbItemIds').append(`<option>${item.code}</option>`);
     }
 }
 
 $('#cmbCustomerIDS').change(function () {
-    let cusID=$('#cmbCustomerIDS').val()
+    let cusID=$('#cmbCustomerIDS').val();
     let customer = searchCustomerForInputField(cusID);
     if (customer != null) {
 
@@ -23,8 +25,23 @@ $('#cmbCustomerIDS').change(function () {
         $('#txtCusNameForOrder').val(customer.name);
         $('#txtCusSalaryForOrder').val(customer.salary);
         $('#txtAddressForOrder').val(customer.address);
+
     }
+    textFieldColorChange_customer();
 });
+
+function textFieldColorChange_customer() {
+    let cusId= $('#cmbCustomerIDS').val();
+    if(cusId ==='Select Customer'){
+
+        $("#txtCusIdForOrder,#txtCusNameForOrder,#txtCusSalaryForOrder,#txtAddressForOrder")
+            .css("border","1px solid #ced4da");
+    }else{
+        $("#txtCusIdForOrder,#txtCusNameForOrder,#txtCusSalaryForOrder,#txtAddressForOrder")
+            .css("border","2px solid green");
+    }
+}
+
 
 $('#cmbItemIds').change(function () {
     let code=$('#cmbItemIds').val();
@@ -35,8 +52,24 @@ $('#cmbItemIds').change(function () {
         $('#txtItemNameForOrder').val(item.name);
         $('#txtItemPriceForOrder').val(item.price);
         $('#txtQTYONHand').val(item.qty);
+    }
+    textFieldColorChange_Item();
+});
+
+
+
+
+function textFieldColorChange_Item() {
+    let itemCode=$('#cmbItemIds').val();
+
+    if (itemCode==="Select Item") {
+
+        $("#txtItemIDForOrder,#txtItemNameForOrder,#txtItemPriceForOrder,#txtQTYONHand")
+            .css("border","1px solid #ced4da");
+
+    }else{
+        $("#txtItemIDForOrder,#txtItemNameForOrder,#txtItemPriceForOrder,#txtQTYONHand")
+            .css("border","2px solid green");
 
     }
-})
-
-
+}
