a449dec058cd00fef7cc790059789cf82269b128
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-10-12 14:29:07 +0530

    Refactoured load orderDetailsTable..

--- assignments/JS/SPA/controller/OrderDetailsController.js
+++ assignments/JS/SPA/controller/OrderDetailsController.js
@@ -17,5 +17,19 @@ function loadOrderTable() {
 function loadOrderDetailsTable() {
     $('#orderDetailTable').empty();
 
+    for (var d of orderDetails) {
+        let row2 = `<tr>
+        <td>${d.orderID}</td>
+        <td>${d.date}</td>
+        <td>${d.customerId}</td>
+        <td>${d.customerName}</td>
+        <td>${d.itemCode}</td>
+        <td>${d.itemName}</td>
+        <td>${d.orderQty}</td>
+        <td>${d.discount}</td>
+        <td>${d.fullTotal}</td>
+        </tr>`;
 
+        $('#orderDetailTable').append(row2);
+    }
 }
